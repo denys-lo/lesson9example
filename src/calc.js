e3bf72f823572b3d0a52fd1ec36b8de5755309e3
denys-lo <div.p.div@gmail.com>
2022-12-26 15:30:21 +0100

    start implementing calculator

---
+++ src/calc.js
@@ -0,0 +1,14 @@
+function calc(operand1, operand2, operation) {
+    let result = 0;
+    switch (operation) {
+        case '+':
+            result = +operand1 + +operand2;
+            break;
+        case '-':
+            result = operand1 - operand2;
+            break;
+        default:
+            break;
+    };
+    console.log(result);
+};
